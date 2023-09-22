24f157d38eed9b53f268aef6adaa5958ee34d654
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-22 12:54:52 -0400

    Create tl_lead_export.php

---
+++ system/modules/contao_leads_export_filepath/languages/en/tl_lead_export.php
@@ -0,0 +1,14 @@
+<?php
+
+  /**
+  * Bright Cloud Studio's Contao Leads Export Filepath
+  *
+  * Copyright (C) 2023 Bright Cloud Studio
+  *
+  * @package    bright-cloud-studio/contao-leads-export-filepath
+  * @link       https://www.brightcloudstudio.com/
+  * @license    http://opensource.org/licenses/lgpl-3.0.html
+  **/
+  
+  /* Fields */
+  $GLOBALS['TL_LANG']['tl_lead_export']['full_path']          = array('Export Full Filepath', 'Check this to have File Upload fields export as fullpath URLs');
